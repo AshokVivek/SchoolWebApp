f2e72c1041a0a55301d8a5b9f8166cb24b2e38fd
Vivek <vivekpal1312@gmail.com>
2023-09-24 05:38:11 +0530

    final changes

--- handler.go
+++ handler.go
@@ -2,73 +2,144 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	var students []Student
+	result, err := db.Query("SELECT id, name, marks from students")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var student Student
+		err := result.Scan(&student.ID, &student.Name, &student.Marks)
+		if err != nil {
+			panic(err.Error())
+		}
+		students = append(students, student)
+	}
+
 	json.NewEncoder(w).Encode(students)
 }
 
 func getStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range students {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			break
-		}
-		return
+	studentID := params["id"]
+
+	result, err := db.Query("SELECT id, name, marks from students where id=?", studentID)
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(&Student{})
+	defer result.Close()
+
+	var student Student
+	if result.Next() {
+		err = result.Scan(&student.ID, &student.Name, &student.Marks)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		json.NewEncoder(w).Encode(student)
+		fmt.Fprintf(w, "Student with ID %s fetched successfully", studentID)
+	} else {
+		fmt.Fprintf(w, "There is no student with ID %s", studentID)
+	}
+}
+
+func fetchTopper(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	result, err := db.Query("SELECT ID, Name, Marks FROM students ORDER BY Marks DESC LIMIT 1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var topperStudent Student
+	if result.Next() {
+		err = result.Scan(&topperStudent.ID, &topperStudent.Name, &topperStudent.Marks)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		json.NewEncoder(w).Encode(topperStudent)
+		fmt.Fprintf(w, "Topper student ID is %s", topperStudent.ID)
+	} else {
+		fmt.Fprintf(w, "There is no students")
+	}
+
+	defer result.Close()
 }
 
 func includeStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
 	var newStudent Student
-	_ = json.NewDecoder(r.Body).Decode(&newStudent)
+	err := json.NewDecoder(r.Body).Decode(&newStudent)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
 
-	students = append(students, newStudent)
-	json.NewEncoder(w).Encode(&newStudent)
+	insertStatement, err := db.Prepare("INSERT INTO students (id, name, marks) VALUES (?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = insertStatement.Exec(newStudent.ID, newStudent.Name, newStudent.Marks)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprint(w, "Student included successfully")
 }
 
 func updateStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	studentMarks, err := strconv.Atoi(params["marks"])
+	studentID := params["id"]
+
+	var updatedStudent Student
+	err := json.NewDecoder(r.Body).Decode(&updatedStudent)
 	if err != nil {
-		http.Error(w, "Invalid marks", http.StatusBadRequest)
+		log.Println(err)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
-	for index, item := range students {
-		if item.ID == params["id"] {
-			students = append(students[:index], students[index+1:]...)
 
-			var student Student
-			_ = json.NewDecoder(r.Body).Decode(&student)
-			student.ID = params["id"]
-			student.Name = params["name"]
-			student.Marks = studentMarks
-			students = append(students, student)
-			json.NewEncoder(w).Encode(&student)
-
-			return
-		}
+	_, err = db.Exec(
+		"UPDATE students SET id=?, name=?, marks=? where id=?",
+		updatedStudent.ID,
+		updatedStudent.Name,
+		updatedStudent.Marks,
+		studentID,
+	)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Failed to update student detail", http.StatusInternalServerError)
+		return
 	}
-	json.NewEncoder(w).Encode(&students)
+
+	fmt.Fprintf(w, "Student with ID %s updated successfully", updatedStudent.ID)
 }
 
 func deleteStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range students {
-		if item.ID == params["id"] {
-			students = append(students[:index], students[index+1:]...)
-			break
-		}
+	studentID := params["id"]
+
+	_, err := db.Exec("DELETE FROM students WHERE id=?", studentID)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Failed to delete student", http.StatusInternalServerError)
+		return
 	}
-	json.NewEncoder(w).Encode(students)
+
+	fmt.Fprintf(w, "Student with ID %s deleted successfully", studentID)
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -12,16 +14,21 @@ type Student struct {
 	Marks int    `json:"marks"`
 }
 
-var students []Student
+var db *sql.DB
+var err error
 
 func main() {
-	router := mux.NewRouter()
+	db, err = sql.Open("mysql", "root:Pendrive@123@tcp(127.0.0.1:3306)/schoolwebapp")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-	students = append(students, Student{ID: "1001", Name: "Soham Sinha", Marks: 476})
-	students = append(students, Student{ID: "1002", Name: "Rahul Majumdar", Marks: 472})
+	router := mux.NewRouter()
 
 	router.HandleFunc("/students", getStudents).Methods("GET")
 	router.HandleFunc("/students", includeStudent).Methods("POST")
+	router.HandleFunc("/students/topper", fetchTopper).Methods("GET")
 	router.HandleFunc("/students/{id}", getStudent).Methods("GET")
 	router.HandleFunc("/students/{id}", updateStudent).Methods("PUT")
 	router.HandleFunc("/students/{id}", deleteStudent).Methods("DELETE")
